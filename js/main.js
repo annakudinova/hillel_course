1872af29ebe6b8d27fae82ead23c0e3b1e4c5a58
annakudinova <235420@duan.edu.ua>
2024-08-15 17:47:19 +0300

    dz18
    dz18

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function() {
+    console.log('Слайдер инициализируется');
+
+    var $slider = $('.team__section-wrapper').slick({
+        dots: false, 
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        customPaging: function(slider, i) {
+            return ''; 
+        }
+    });
+
+  
+    $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+        $('.team__section-dots-dot').removeClass('active');
+        $('.team__section-dots-dot').eq(nextSlide).addClass('active');
+    });
+
+   
+    $('.team__section-dots-dot').on('click', function() {
+        var slideIndex = $(this).data('slide');
+        $slider.slick('slickGoTo', slideIndex);
+        $('.team__section-dots-dot').removeClass('active');
+        $(this).addClass('active');
+    });
+
+   
+    $('.team__section-card').on('click', function() {
+        $slider.slick('slickNext');
+    });
+});
+
+
+
+
+
+
+
+
+
+
